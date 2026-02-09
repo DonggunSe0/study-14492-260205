420ee1fe0fb8db86fd485a341cede68a7029ac62
DonggunSe0 <cloudhenry1@gmail.com>
2026-02-09 17:24:37 +0900

    13-r 목록 테스트

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 
 public class App {
     private Scanner sc;
+    int lastId = 0; //등록번호 증가를 위한 lastId
 
     public App(Scanner sc) {
         this.sc = sc;
@@ -10,22 +11,27 @@ public class App {
     public void run() {
         System.out.println("== 명언 앱=="); //outputStream에 저장됨 다시돌아가바 =>App
 
+
+        //추가 등록을 위한 반복문
+    while (true){
         System.out.println("명령) ");
         String cmd = sc.nextLine();
-
-        System.out.println("1번 명령이 등록 되었습니다.");
-
         switch (cmd){
             case "등록"->{System.out.println("명언 : ");
                 String saying = sc.nextLine();
                 System.out.println("작가 : ");
                 String author = sc.nextLine();
+                lastId++;
+
+                System.out.println("%d번 명령이 등록 되었습니다.".formatted(lastId));
             }
+            case "목록" ->{}
+
             case "종료"->{
                 return ;
             }
 
         }
-        //등록 구현
+    }
     }
 }
