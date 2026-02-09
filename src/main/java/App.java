178f672ffa5f257ac7e93fe1df74f3bfd2996adb
DonggunSe0 <cloudhenry1@gmail.com>
2026-02-09 16:42:19 +0900

    9-b 등록 기능 구현

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -9,5 +9,10 @@ public class App {
 
     public void run() {
         System.out.println("== 명언 앱=="); //outputStream에 저장됨 다시돌아가바 =>App
+
+        System.out.println("명령) ");
+        System.out.println("명언 : ");
+        System.out.println("작가 : ");
+        //등록 구현
     }
 }
